9e581f0cc46d91a8fdc8bdedb9e4ad39676b656c
Wildan G. Budhi <wildangb2@gmail.com>
2019-04-05 13:54:52 +0700

    Bismillah

--- soal2/server-pembeli.c
+++ soal2/server-pembeli.c
@@ -43,8 +43,8 @@ int main(int argc, char const *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) {
-        perror("accept");
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) > 1) {
+        perror("More one Client Connected");
         exit(EXIT_FAILURE);
     }
 
